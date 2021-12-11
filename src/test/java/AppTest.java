93d2b91620c9db117c376bf79a2b12cef7a0b419
BSManohar <manohar.bs909@gmail.com>
2021-12-11 08:51:45 +0000

    JIRA - 1234

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void test1(){
+    System.out.println("Running test 1");
+  }
+
+  @Test
+  public void test2(){
+    System.out.println("Running test 2");
+  }
+
+  @Test
+  public void test3(){
+    System.out.println("Running test 3");
+  }
+
+
+
+
+}
